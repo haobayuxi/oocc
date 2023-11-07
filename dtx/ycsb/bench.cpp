30dac0fdddc434cd9345be464dea92b6e7252ab3
haobayuxi <whaohit@gmail.com>
2023-11-07 21:37:17 +0800

    update

--- dtx/ycsb/bench.cpp
+++ dtx/ycsb/bench.cpp
@@ -58,7 +58,7 @@ bool TxYCSB(tx_id_t tx_id, DTX *dtx) {
     if (read_only || i % 2 == 1) {
       dtx->AddToReadOnlySet(micro_obj);
 
-      SDS_INFO("txn read key = %ld", micro_key.item_key);
+      // SDS_INFO("txn read key = %ld", micro_key.item_key);
     } else {
       dtx->AddToReadWriteSet(micro_obj);
     }
